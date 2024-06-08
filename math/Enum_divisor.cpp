520694c38b3e28be03f06f6877afac5104609da5
WINGU03 <hatel0326@gmail.com>
2024-06-08 20:27:55 +0900

    long long を ll に変更

--- math/Enum_divisor.cpp
+++ math/Enum_divisor.cpp
@@ -1,16 +1,12 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-
-std::vector<long long> enum_divisor(long long n) {
-    std::vector<long long> res;
-    for (long long i = 1LL; i * i <= n; ++i) {
+vector<ll> enum_divisor(ll n) {
+    vector<ll> res;
+    for (ll i = 1; i * i <= n; ++i) {
         if (n % i == 0) {
             res.push_back(i);
-            long long j = n / i;
+            ll j = n / i;
             if (j != i) res.push_back(j);
         }
     }
-    std::sort(res.begin(), res.end());
+    sort(res.begin(), res.end());
     return res;
 }
--- math/Prime_factorize.cpp
+++ math/Prime_factorize.cpp
@@ -1,17 +1,14 @@
-#include <iostream>
-#include <vector>
-
-std::vector<std::pair<long long, long long> > prime_factorize(long long n) {
-    std::vector<std::pair<long long, long long> > res;
-    for (long long p = 2; p * p <= n; ++p) {
+vector<P> prime_factorize(ll n) {
+    vector<P> res;
+    for (ll p = 2; p * p <= n; ++p) {
         if (n % p != 0) continue;
         int num = 0;
         while (n % p == 0) {
             ++num;
             n /= p;
         }
-        res.push_back(std::make_pair(p, num));
+        res.push_back(make_pair(p, num));
     }
-    if (n != 1) res.push_back(std::make_pair(n, 1));
+    if (n != 1) res.push_back(make_pair(n, 1));
     return res;
 }
