801cb0f656b4358a21063e1e021bdf7ae4330826
WINGU03 <hatel0326@gmail.com>
2024-06-09 16:01:41 +0900

    CompileOption_verify追加

---
+++ verify/CompileOption.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/many_aplusb"
+
+#include <bits/stdc++.h>
+#include "other/Macro"
+#include "other/CompileOption"
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        ll a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
