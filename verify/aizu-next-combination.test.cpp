debbfe9e86cb9c120a3ce98788d953aa5937ee36
WINGU03 <hatel0326@gmail.com>
2024-06-18 21:44:21 +0900

    udpate

--- verify/aizu-next-combination.test.cpp
+++ verify/aizu-next-combination.test.cpp
@@ -2,30 +2,7 @@
 
 #include <bits/stdc++.h>
 #include "template.hpp"
-
-template <typename T>
-bool next_combination(const T first, const T last, int k) {
-    const T subset = first + k;
-    if (first == last || first == subset || last == subset) {
-        return false;
-    }
-    T src = subset;
-    while (first != src) {
-        src--;
-        if (*src < *(last - 1)) {
-            T dest = subset;
-            while (*src >= *dest) {
-                dest++;
-            }
-            iter_swap(src, dest);
-            rotate(src + 1, dest + 1, last);
-            rotate(subset, subset + (last - dest) - 1, last);
-            return true;
-        }
-    }
-    rotate(first, subset, last);
-    return false;
-}
+#include "other/next_combination.hpp"
 
 int main() {
     int n, s;
