217e4e30d9195eddef1ed78c9857a2d29a8483cb
WINGU03 <142299476+WINGU03@users.noreply.github.com>
2024-06-22 09:18:57 +0900

    update

--- docs/mo.md
+++ docs/mo.md
@@ -2,3 +2,30 @@
 title: Mo’s algorithm
 documentation_of: ./misc/mo.hpp
 ---
+
+# コンストラクタ
+
+```cpp
+
+struct M {
+    using T = ;
+    static void add_left(int pos) {
+        
+    }
+    static void add_right(int pos) {
+        
+    }
+    static void del_left(int pos) {
+        
+    }
+    static void del_right(int pos) {
+        
+    }
+    static T res(int i) {
+
+    }
+};
+
+Mo<M> mo(N, Q);
+
+```
--- misc/mo.hpp
+++ misc/mo.hpp
@@ -8,31 +8,31 @@ struct Mo {
         backet = max<int>(1, (double)(N) / max<double>(1, sqrt(Q * 2.0 / 3)));
         iota(order.begin(), order.end(), 0);
     }
-    void add_query(int lf, int ri) {
-        left.emplace_back(lf);
-        right.emplace_back(ri);
+    void add_query(int left_id, int right_id) {
+        left.emplace_back(left_id);
+        right.emplace_back(right_id);
     }
     vector<T> run() {
         vector<T> answer(order.size());
         sort(order.begin(), order.end(), [&](int a, int b) {
-            int ab = left[a] / backet, bb = left[b] / backet;
-            if (ab != bb) return ab < bb;
-            if (ab & 1) return right[a] < right[b];
+            int a_block = left[a] / backet, b_block = left[b] / backet;
+            if (a_block != b_block) return a_block < b_block;
+            if (a_block & 1) return right[a] < right[b];
             return right[a] > right[b];
         });
-        int nl = 0, nr = 0;
+        int now_left = 0, now_right = 0;
         for (int i : order) {
-            while (nl > left[i]) {
-                M::add_left(--nl);
+            while (now_left > left[i]) {
+                M::add_left(--now_left);
             }
-            while (right[i] > nr) {
-                M::add_right(nr++);
+            while (right[i] > now_right) {
+                M::add_right(now_right++);
             }
-            while (nl < left[i]) {
-                M::del_left(nl++);
+            while (now_left < left[i]) {
+                M::del_left(now_left++);
             }
-            while (right[i] < nr) {
-                M::del_right(--nr);
+            while (right[i] < now_right) {
+                M::del_right(--now_right);
             }
             answer[i] = M::res(i);
         }
