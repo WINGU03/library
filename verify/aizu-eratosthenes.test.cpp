8ea97198e9851771c7b6edf66009e8f45d6261e8
WINGU03 <wingu0326@gmail.com>
2024-08-18 00:49:59 +0900

    update

--- verify/aizu-eratosthenes.test.cpp
+++ verify/aizu-eratosthenes.test.cpp
@@ -1,20 +1,19 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/0009"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/ALDS1_1_C"
 
 #include <bits/stdc++.h>
 #include "template.hpp"
 #include "math/eratosthenes.hpp"
 
-using P = pair<int, int>;
-using T = tuple<int, int, int>;
 int main() {
     int n;
-    while (cin >> n) {
-        auto p = sieve(n);
-        int ans = 0;
-        for (int i = 0; i <= n; i++) {
-            if (p[i]) ans++;
-        }
-        cout << ans << endl;
+    cin >> n;
+    auto p = sieve((int)1e8);
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (p[x]) ans++;
     }
+    cout << ans << endl;
     return 0;
 }
