3d91ab2c280a4310386121a2fae1ef94a97b0e1b
WINGU03 <wingu0326@gmail.com>
2024-08-23 17:22:06 +0900

    update

--- misc/heuristic-template.hpp
+++ misc/heuristic-template.hpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-using namespace chrono;
+#include <atcoder/all>
+using namespace atcoder;
 #ifdef DEFINED_ONLY_IN_LOCAL
 #include <dump.hpp>
 #define dump(...) cpp_dump(__VA_ARGS__)
@@ -8,27 +9,35 @@ using namespace chrono;
 #undef dump
 #define dump(...)
 #endif
-template<class T>inline bool chmax(T &a,T b){if(a<b){a=b;return true;}return false;}
-template<class T>inline bool chmin(T &a,T b){if(a>b){a=b;return true;}return false;}
-#define rep1(i,a) for(int i=0;i<(int)(a);i++)
-#define rep2(i,a,b) for(int i =(int)(a);i<(int)(b);i++)
-#define rep3(i,a,b,c) for(int i=(int)(a);i<(int)(b);i+=(int)(c))
-#define overloadRep(a,b,c,d,e,...)e
-#define rep(...) overloadRep(__VA_ARGS__,rep3,rep2,rep1)(__VA_ARGS__)
-#define rrep(i,a,b) for(int i=(int)(a);i<=(int)(b);i++)
-#define drep(i,a,b) for(int i=(int)(a);i>=(int)(b);i--)
-#define all(a) a.begin(),a.end()
-#define rall(a) a.rbegin(),a.rend()
+#define rep1(i, a) for (int i = 0; i < (int)(a); i++)
+#define rep2(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define rep3(i, a, b, c) for (int i = (int)(a); i < (int)(b); i += (int)(c))
+#define overloadRep(a, b, c, d, e, ...) e
+#define rep(...) overloadRep(__VA_ARGS__, rep3, rep2, rep1)(__VA_ARGS__)
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
 using ll = long long;
-const int inf=1e9;
-const ll INF=1e18;
-const int dx[4]={0,1,0,-1};
-const int dy[4]={1,0,-1,0};
-const int ddx[8]={1,1,1,0,-1,-1,-1,0};
-const int ddy[8]={1,0,-1,-1,-1,0,1,1};
-bool bit(ll x, int p){return (x>>p)&1;}
-bool out(int ni, int nj, int h, int w){return (ni<0 or ni>=h or nj<0 or nj>=w);}
-int pc(ll x){return __builtin_popcountll(x);}
+using ull = unsigned long long;
+const int inf = 1e9;
+const ll INF = 1e18;
+const int dx[4] = {0, 1, 0, -1};
+const int dy[4] = {1, 0, -1, 0};
+const int ddx[8] = {1, 0, -1, 0, 1, -1, 1, -1};
+const int ddy[8] = {0, 1, 0, -1, 1, -1, -1, 1};
+template <class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
+template <class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
+template <class T1, class T2> istream& operator>>(istream& is, pair<T1, T2>& p) {is >> p.first >> p.second; return is;}
+template <class T1, class T2> ostream& operator<<(ostream& os, const pair<T1, T2>& p) {os << p.first << " " << p.second << '\n'; return os;}
+template <class T> istream& operator>>(istream& is, vector<T>& v) {for (T& in : v) {is >> in;} return is;}
+template <class T> ostream& operator<<(ostream& os, const vector<T>& v) {rep(i, (int)v.size()) {os << v[i] << " \n"[i + 1 == (int)v.size()];} return os;}
+template <class T> istream& operator>>(istream& is, vector<vector<T>>& vv) {for (vector<T>& v : vv) {is >> v;} return is;}
+template <class T> ostream& operator<<(ostream& os, vector<vector<T>>& vv) {for (vector<T>& v : vv) {os << v;} return os;}
+inline bool bit(ll x, int p) {return (x >> p) & 1;}
+inline bool out(int ni, int nj, int h, int w) {return (ni < 0 or ni >= h or nj < 0 or nj >= w);}
+inline int pc(ll x) {return __builtin_popcountll(x);}
+template <class T> inline T max(vector<T> x) {return *max_element(x.begin(), x.end());}
+template <class T> inline T min(vector<T> x) {return *min_element(x.begin(), x.end());}
+template <class T> inline T sum(vector<T> x) {return reduce(x.begin(), x.end());}
 class Random {
    public:
     mt19937 mt;
@@ -39,6 +48,7 @@ class Random {
     inline double nextLog() {return log(dd_(mt));}
 };
 Random rnd{};
+using namespace chrono;
 using time_data = high_resolution_clock::time_point;
 #define now_time high_resolution_clock::now()
 #define change_mil(a) duration_cast<milliseconds>(a).count()
