f1a8dd7aa45a6d32e9b3c28de85e9a2fad9a2e75
WINGU03 <wingu0326@gmail.com>
2024-09-20 17:11:59 +0900

    update

---
+++ verify/aizu-combination.test.cpp
@@ -0,0 +1,15 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/5/DPL_5_E"
+
+#include <bits/stdc++.h>
+#include "template.hpp"
+
+using mint = modint1000000007;
+#include "math/combination.hpp"
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    combination c(k);
+    cout << c.com(k, n).val() << endl;
+    return 0;
+}
