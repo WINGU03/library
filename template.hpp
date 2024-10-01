3e57ab821c91d88aa04831437975507404309bdf
WINGU03 <wingu0326@gmail.com>
2024-10-01 21:17:41 +0900

    update

--- template.hpp
+++ template.hpp
@@ -9,42 +9,5 @@ using namespace atcoder;
 #undef dump
 #define dump(...)
 #endif
-#define rep1(a) for (int i = 0; i < a; i++)
-#define rep2(i, a) for (int i = 0; i < a; i++)
-#define rep3(i, a, b) for (int i = a; i < b; i++)
-#define rep4(i, a, b, c) for (int i = a; i < b; i += c)
-#define overloadRep(a, b, c, d, e, ...) e
-#define rep(...) overloadRep(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
-#define all(a) a.begin(), a.end()
-#define rall(a) a.rbegin(), a.rend()
-#define endl '\n'
-#define INT(...) int __VA_ARGS__; input(__VA_ARGS__)
-#define LL(...) ll __VA_ARGS__; input(__VA_ARGS__)
-using ll = long long;
-using ull = unsigned long long;
-const int inf = 1e9;
-const ll INF = 1e18;
-const int dx[4] = {0, 1, 0, -1};
-const int dy[4] = {1, 0, -1, 0};
-const int ddx[8] = {1, 0, -1, 0, 1, -1, 1, -1};
-const int ddy[8] = {0, 1, 0, -1, 1, -1, -1, 1};
-struct cincout {cincout() {ios_base::sync_with_stdio(false); cin.tie(nullptr); cout << fixed << setprecision(15);}} init;
-template <class... T> void input(T&... a) {(cin >> ... >> a);}
-template <class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
-template <class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
-template <class T1, class T2> istream& operator>>(istream& is, pair<T1, T2>& p) {is >> p.first >> p.second; return is;}
-template <class T1, class T2> ostream& operator<<(ostream& os, const pair<T1, T2>& p) {os << p.first << " " << p.second << '\n'; return os;}
-template <class T> istream& operator>>(istream& is, vector<T>& v) {for (T& in : v) {is >> in;} return is;}
-template <class T> ostream& operator<<(ostream& os, const vector<T>& v) {rep(i, (int)v.size()) {os << v[i] << " \n"[i + 1 == (int)v.size()];} return os;}
-template <class T> istream& operator>>(istream& is, vector<vector<T>>& vv) {for (vector<T>& v : vv) {is >> v;} return is;}
-template <class T> ostream& operator<<(ostream& os, vector<vector<T>>& vv) {for (vector<T>& v : vv) {os << v;} return os;}
-template <class T> inline T max(vector<T> x) {return *max_element(x.begin(), x.end());}
-template <class T> inline T min(vector<T> x) {return *min_element(x.begin(), x.end());}
-template <class T> inline T sum(vector<T> x) {return reduce(x.begin(), x.end());}
-template <class... T> constexpr auto min(T... a) {return min(initializer_list<common_type_t<T...>>{a...});}
-template <class... T> constexpr auto max(T... a) {return max(initializer_list<common_type_t<T...>>{a...});}
-inline bool bit(ll x, int p) {return (x >> p) & 1;}
-inline bool out(int ni, int nj, int h, int w) {return (ni < 0 or ni >= h or nj < 0 or nj >= w);}
-inline int pc(ll x) {return __builtin_popcountll(x);}
-void Yes(bool judge = true) {cout << (judge ? "Yes" : "No") << endl;}
-void No(bool judge = true) {cout << (judge ? "No" : "Yes") << endl;}
+#define rep(i, n) for (int i = 0; i < n; i++)
+template <class T> istream& operator>>(istream& I, vector<T>& V) { for (T& X : V) I >> X; return I; }
