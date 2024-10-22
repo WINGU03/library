cb3688a7b96ed7cf764b971883f50adc9d5a5a7e
WINGU03 <wingu0326@gmail.com>
2024-10-22 20:37:22 +0900

    udpate

--- template.hpp
+++ template.hpp
@@ -2,11 +2,10 @@
 using namespace std;
 #include <atcoder/modint>
 using namespace atcoder;
-#ifdef DEFINED_ONLY_IN_LOCAL
-#include <dump.hpp>
-#define dump(...) cpp_dump(__VA_ARGS__)
+#ifdef local
+#include <cpp-dump.hpp>
+#define dump cpp_dump
 #else
-#undef dump
 #define dump(...)
 #endif
 #define rep(i, n) for (int i = 0; i < n; i++)
