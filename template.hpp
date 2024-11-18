9bc21b2a7a1a8a3e62e8de4e098f1a887c671310
WINGU03 <wingu0326@gmail.com>
2024-11-18 18:40:35 +0900

    update

--- template.hpp
+++ template.hpp
@@ -8,4 +8,6 @@ using namespace atcoder;
 #define debug(...)
 #endif
 #define rep(i, n) for (int i = 0; i < n; i++)
-template <class T> istream& operator>>(istream& I, vector<T>& V) { for (T& X : V) I >> X; return I; }
+template <class T> istream& operator>>(istream& I, vector<T>& V) {for (T& X : V) I >> X; return I;}
+template <class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
+template <class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
