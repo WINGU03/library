aafaf2ce5849b76b2410d63627896081cb23f757
WINGU03 <wingu0326@gmail.com>
2024-12-11 21:51:41 +0900

    update

--- template.hpp
+++ template.hpp
@@ -11,3 +11,4 @@ using namespace atcoder;
 template <class T> istream& operator>>(istream& I, vector<T>& V) {for (T& X : V) I >> X; return I;}
 template <class T> inline bool chmax(T& a, T b) {if (a < b) {a = b; return true;} return false;}
 template <class T> inline bool chmin(T& a, T b) {if (a > b) {a = b; return true;} return false;}
+const int dx[](1, 0, -1, 0), dy[](0, 1, 0, -1), inf = 2e9; const long INF = 1e18;
